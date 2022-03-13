c848cd6c6dc3b2362e2583626819332f117fed19
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2022-03-13 14:21:12 +0000

    Initialize GS

--- InvincibilityMonitor/InvincibilityMonitor.cs
+++ InvincibilityMonitor/InvincibilityMonitor.cs
@@ -14,7 +14,7 @@ namespace InvincibilityMonitor
 
         public InvincibilityMonitor() : base(null) { Instance = this; }
 
-        public static GlobalSettings GS;
+        public static GlobalSettings GS = new();
         public void OnLoadGlobal(GlobalSettings gs) => GS = gs;
         public GlobalSettings OnSaveGlobal() => GS;
 
@@ -40,36 +40,5 @@ namespace InvincibilityMonitor
                 : InvincibilityCondition.TimerSafety ? "Waiting"
                 : "Vulnerable";
         }
-    }
-
-    internal static class DebugMod
-    {
-        [ModImportName("DebugMod")]
-        private static class DebugImport
-        {
-            public static Func<bool, string> GetStringForBool = null;
-            public static Action<string, bool> CreateCustomInfoPanel = null;
-            public static Action<string, float, float, float, string, Func<string>> AddInfoToPanel = null;
-            public static Action<string, float> CreateSimpleInfoPanel = null;
-            public static Action<string, string, Func<string>> AddInfoToSimplePanel = null;
-        }
-        static DebugMod()
-        {
-            typeof(DebugImport).ModInterop();
-        }
-
-        public static string GetStringForBool(bool b) => DebugImport.GetStringForBool?.Invoke(b) ?? "";
-
-        public static void CreateCustomInfoPanel(string Name, bool ShowSprite)
-            => DebugImport.CreateCustomInfoPanel?.Invoke(Name, ShowSprite);
-
-        public static void AddInfoToPanel(string Name, float xLabel, float xInfo, float y, string label, Func<string> textFunc)
-            => DebugImport.AddInfoToPanel?.Invoke(Name, xLabel, xInfo, y, label, textFunc);
-
-        public static void CreateSimpleInfoPanel(string Name, float sep)
-            => DebugImport.CreateSimpleInfoPanel?.Invoke(Name, sep);
-
-        public static void AddInfoToSimplePanel(string Name, string label, Func<string> textFunc)
-            => DebugImport.AddInfoToSimplePanel?.Invoke(Name, label, textFunc);
     }
 }
